a113c0ac14216401ee680580eea06312988a17d1
jiohlee <wldh23@gmail.com>
2023-06-16 15:54:48 +0900

    created nignx & modified settings

--- frontend/src/mainpage.svelte
+++ frontend/src/mainpage.svelte
@@ -1,9 +1,7 @@
 <script>
 	import Header from "./header.svelte";
 	import Contents from "./contents.svelte";
-	import Footer from "./footer.svelte";
 </script>
 
 <Header />
 <Contents />
-<Footer />
